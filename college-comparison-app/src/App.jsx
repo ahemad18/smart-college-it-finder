66957d563f076c1ed33319bd79ff9c872dbf0c64
ahemad18 <ahemad18@github.com>
2026-04-06 14:23:31 -0400

    fix: use HashRouter and set Vite base for GitHub Pages

--- college-comparison-app/src/App.jsx
+++ college-comparison-app/src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import CollegeComparison from './CollegeComparison';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/compare" element={<CollegeComparison />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
